7d5c03ca30aed10d3596a7dfd972db5882c3a729
NeethaUPrabhu <neethaprabhu@gmail.com>
2024-06-26 01:42:05 +0530

    Coin Collector Jungle Maze scene created
    Coin Collector Jungle Maze scene created

---
+++ Assets/Scripts/Gameplay/PlayerInput/CoinManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoinManager : MonoBehaviour
+{
+    public int coinCount; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Debug.Log("In CoinManager Start Coin Count: \n" + coinCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("In CoinManager update Coin Count: \n" + coinCount);
+
+    }
+    
+}
+
--- Assets/Scripts/Gameplay/PlayerInput/PlayerMovementSlam.cs
+++ Assets/Scripts/Gameplay/PlayerInput/PlayerMovementSlam.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
+
+
 
 namespace Scripts.Gameplay.PlayerInput
 {
     // Fully respect the player's movement commands.  Acceleration is instantaneous.
     public class PlayerMovementSlam : PlayerMovementBase
     {
+        public CoinManager cm;
         public float maxSpeed = 30f;
         [Tooltip("How quickly the player can change their motion.  Note that this value is also used to stop the player when they are not trying to move, so you may want to set the rigidbody's Linear Drag to zero.  Otherwise, there are two forces trying to slow the player down.")]
         public float maxAcceleration = 10f;
@@ -12,6 +15,7 @@ namespace Scripts.Gameplay.PlayerInput
 
         protected void FixedUpdate()
         {
+            //Debug.Log("In PlayerMovementSlam FixedUpdate \n");
             var desiredVelocity = this.GetMoveInput() * this.maxSpeed;
             var currentVelocity = this.cachedRigidbody2D.velocity;
             var desiredDeltaVelocity = desiredVelocity - currentVelocity;
@@ -30,5 +34,18 @@ namespace Scripts.Gameplay.PlayerInput
             var force = appliedDeltaVelocity * this.cachedRigidbody2D.mass;
             this.cachedRigidbody2D.AddForce(force, ForceMode2D.Impulse);
         }
+
+       
+             void OnTriggerEnter2D(Collider2D other)
+             {
+                //Debug.Log("COnTriggerEnter2D Tag : \n" + other.gameObject.tag);
+                 if (other.gameObject.tag == "Coin")
+                 {
+                     Destroy(other.gameObject);
+                     cm.coinCount++; // Increment the coin count
+                  //   Debug.Log("Coin Collected\n");
+                  //   Debug.Log("Coin Count: \n" + cm.coinCount);
+                 }
+             }
     }
 }
